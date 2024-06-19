a9d3887eb2309c0e1e21dc9614e4e63e7e130731
Alfonso Sanchis <9105895@alumnos.ufv.es>
2024-06-19 18:27:14 +0200

    Get

--- ASA/src/main/java/es/ufv/dis/final2022/ASA/Controlador.java
+++ ASA/src/main/java/es/ufv/dis/final2022/ASA/Controlador.java
@@ -1,4 +1,17 @@
 package es.ufv.dis.final2022.ASA;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
 public class Controlador {
+
+
+    @GetMapping("/productos")
+    public ArrayList<Productos> productos(){
+        ArrayList<Productos> listaProductos = new jsonReader().leerJson("static/datos.json"); // leemos el fichero json
+        return listaProductos;
+    }
 }
