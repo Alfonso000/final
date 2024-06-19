0be2815281862e49e3ed6e1bb760e84bb01375da
Alfonso Sanchis <9105895@alumnos.ufv.es>
2024-06-19 21:52:42 +0200

    otra mas

--- ASA/src/main/java/es/ufv/dis/final2022/ASA/jsonReader.java
+++ ASA/src/main/java/es/ufv/dis/final2022/ASA/jsonReader.java
@@ -3,11 +3,13 @@ package es.ufv.dis.final2022.ASA;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 
 public class jsonReader {
@@ -36,6 +38,28 @@ public class jsonReader {
         try(FileWriter writer = new FileWriter(fichero)){
             gson.toJson(listaProductos, writer);
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createPDF(Productos producto) {
+        try {
+            Document doc = new Document(PageSize.A4, 50, 50, 100, 72);
+            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream("productos/" + producto.getNombre() + ".pdf"));
+            doc.open();
+            String text = "some padding text";
+            Paragraph p1 = new Paragraph(producto.getNombre());
+            p1.setAlignment(Element.ALIGN_JUSTIFIED);
+            doc.add(p1);
+            p1 = new Paragraph(producto.getCategoria());
+            doc.add(p1);
+            p1 = new Paragraph(producto.getEan13());
+            doc.add(p1);
+            p1 = new Paragraph(producto.getPrecio());
+            doc.add(p1);
+
+            doc.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
