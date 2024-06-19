5c80da7dfdc9d366cbaa7f714a5530d60849ce76
Alfonso Sanchis <9105895@alumnos.ufv.es>
2024-06-19 17:44:42 +0200

    Subida inicial

---
+++ ASA/src/main/java/es/ufv/dis/final2022/ASA/AsaApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final2022.ASA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsaApplication.class, args);
+	}
+
+}
---
+++ ASA/src/test/java/es/ufv/dis/final2022/ASA/AsaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final2022.ASA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
