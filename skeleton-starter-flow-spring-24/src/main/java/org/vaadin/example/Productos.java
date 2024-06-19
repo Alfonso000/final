da1f14fcae2b83bcc2a4f0b58154fe33da8293ed
Alfonso Sanchis <9105895@alumnos.ufv.es>
2024-06-19 19:00:25 +0200

    iniciofront

---
+++ skeleton-starter-flow-spring-24/src/main/java/org/vaadin/example/Productos.java
@@ -0,0 +1,63 @@
+package org.vaadin.example;
+
+
+
+public class Productos {
+    private String nombre;
+    private String categoria;
+    private String ean13;
+    private String precio;
+
+    public Productos() {
+    }
+
+    public Productos(String nombre, String categoria, String ean13, String precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.ean13 = ean13;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getEan13() {
+        return ean13;
+    }
+
+    public void setEan13(String ean13) {
+        this.ean13 = ean13;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Productos{" +
+                "nombre='" + nombre + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", ean13='" + ean13 + '\'' +
+                ", precio='" + precio + '\'' +
+                '}';
+    }
+}
+
